01eee335c03f93cc7427771cf6b2ff1276b5e6de
Shaun Moss <shaun@astromultimedia.com>
2024-06-17 11:09:08 +1000

    Fixed CSS issues with log table.

--- wp-logify/assets/js/admin.js
+++ wp-logify/assets/js/admin.js
@@ -2,41 +2,45 @@ jQuery(($) => {
     console.log('Admin.js loaded'); // Debug: Check if the script is loaded
 
     let table = $('#wp-logify-activity-log').DataTable({
-        "processing": true,
-        "language": {
+        processing: true,
+        language: {
             processing: 'Please wait...'
         },
-        "serverSide": true,
-        "ajax": {
-            "url": wpLogifyAdmin.ajaxurl,
-            "type": "POST",
-            "data": (d) => {
+        serverSide: true,
+        ajax: {
+            url: wpLogifyAdmin.ajaxurl,
+            type: "POST",
+            data: (d) => {
                 d.action = 'wp_logify_fetch_logs';
                 console.log('Sending AJAX request with data:', d); // Debug: Check data sent with the request
             },
-            "error": (xhr, error, code) => {
+            error: (xhr, error, code) => {
                 console.log('AJAX error:', error);
                 console.log('AJAX error code:', code);
             }
         },
-        "columns": [
-            { "data": "id" },
-            { "data": "date_time" },
-            { "data": "user" },
-            { "data": "source_ip" },
-            { "data": "event_type" },
-            { "data": "object" },
+        columns: [
+            {
+                data: "id",
+                width: '70px'
+            },
+            { data: "date_time" },
+            { data: "user" },
+            { data: "source_ip" },
+            { data: "event_type" },
+            { data: "object" },
             {
                 className: 'details-control',
                 orderable: false,
-                defaultContent: '<span class="wp-logify-show-details">Show</span>'
+                defaultContent: '<span class="wp-logify-show-details">Show</span>',
+                width: '100px'
             }
         ],
-        "order": [[1, 'desc']],
-        "searching": true,
-        "paging": true,
-        "pageLength": 20,
-        "lengthMenu": [10, 20, 50, 100]
+        order: [[1, 'desc']],
+        searching: true,
+        paging: true,
+        pageLength: 20,
+        lengthMenu: [10, 20, 50, 100]
     });
 
     // Custom search box
@@ -68,8 +72,6 @@ jQuery(($) => {
         }
         else {
             // Open this row.
-            console.log(row);
-            console.log(row.data());
             row.child(row.data().details, 'details-row').show();
             tr.find('td.details-control span').text('Hide');
         }
