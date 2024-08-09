6392fddf4cf16ee6ae8618b9ee4157e080de301a
Shaun Moss <shaun@astromultimedia.com>
2024-08-09 14:43:50 +1000

    Updated SCSS with colours and common classes for tags.

---
+++ old/my-single-file-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: My Single File Plugin
+ * Plugin URI:  https://example.com/my-single-file-plugin
+ * Description: A simple WordPress plugin that does something cool.
+ * Version:     1.0
+ * Author:      Your Name
+ * Author URI:  https://example.com
+ * License:     GPLv2 or later
+ */
+
+// Your plugin code goes here.
+
+function my_single_file_plugin_function() {
+	echo 'Hello from my single-file plugin!';
+}
+add_action( 'wp_footer', 'my_single_file_plugin_function' );
