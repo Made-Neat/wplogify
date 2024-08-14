5f44642fb5ab3766ef2445cab25463b280740ded
Shaun Moss <shaun@astromultimedia.com>
2024-08-14 17:14:15 +1000

    Updated version number.

--- wp-logify/wp-logify.php
+++ wp-logify/wp-logify.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Logify
  * Plugin URI: https://wplogify.com
  * Description: WP Logify features advanced tracking to ensure awareness of all changes made to your WordPress website, including who made them and when.
- * Version: 1.21
+ * Version: 1.22
  * Author: Made Neat
  * Author URI: https://madeneat.com.au
  * License: GPL2
