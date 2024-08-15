4658add8bfca2708ef36da782309c91ab7fe14a2
Shaun Moss <shaun@astromultimedia.com>
2024-08-16 08:07:27 +1000

    Reorganised classes and releases.

--- wp-logify/wp-logify.php
+++ wp-logify/wp-logify.php
@@ -34,52 +34,48 @@ define( 'WP_LOGIFY_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'WP_LOGIFY_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 // Include all include files.
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/database/class-database.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-admin.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-cron.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-database.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-log-page.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-datetimes.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-logger.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-main.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-object-reference.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-plugin-settings.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-serialization.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-types.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-urls.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/misc/class-widget.php';
 
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/database/models/class-event.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/database/models/class-eventmeta.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/database/models/class-property.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/models/class-event.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/models/class-eventmeta.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/models/class-property.php';
 
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/database/repositories/class-repository.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/database/repositories/class-event-repository.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/database/repositories/class-eventmeta-repository.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/database/repositories/class-property-repository.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/repositories/class-repository.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/repositories/class-event-repository.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/repositories/class-eventmeta-repository.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/repositories/class-property-repository.php';
 
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/class-object-reference.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/trackers/class-object-tracker.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/trackers/class-comment-tracker.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/trackers/class-core-tracker.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/trackers/class-option-tracker.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/trackers/class-plugin-tracker.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/trackers/class-post-tracker.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/trackers/class-term-tracker.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/trackers/class-theme-tracker.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/trackers/class-user-tracker.php';
 
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/utilities/class-object-utility.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/utilities/class-comment-utility.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/utilities/class-core-utility.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/utilities/class-option-utility.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/utilities/class-plugin-utility.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/utilities/class-post-utility.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/utilities/class-term-utility.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/utilities/class-theme-utility.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/utilities/class-user-utility.php';
-
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/trackers/class-object-tracker.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/trackers/class-comment-tracker.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/trackers/class-core-tracker.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/trackers/class-option-tracker.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/trackers/class-plugin-tracker.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/trackers/class-post-tracker.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/trackers/class-term-tracker.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/trackers/class-theme-tracker.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/objects/trackers/class-user-tracker.php';
-
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/ui/class-admin.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/ui/class-log-page.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/ui/class-plugin-settings.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/ui/class-widget.php';
-
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utility/class-datetimes.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utility/class-serialization.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utility/class-types.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utility/class-urls.php';
-
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/class-cron.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/class-logger.php';
-require_once WP_LOGIFY_PLUGIN_DIR . 'includes/class-main.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utilities/class-object-utility.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utilities/class-comment-utility.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utilities/class-core-utility.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utilities/class-option-utility.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utilities/class-plugin-utility.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utilities/class-post-utility.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utilities/class-term-utility.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utilities/class-theme-utility.php';
+require_once WP_LOGIFY_PLUGIN_DIR . 'includes/utilities/class-user-utility.php';
 
 require_once WP_LOGIFY_PLUGIN_DIR . 'includes/debug.php';
 // require_once WP_LOGIFY_PLUGIN_DIR . 'includes/test.php';
