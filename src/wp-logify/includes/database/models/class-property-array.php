bb39dbb695707031ff20a33c2faabafa47e402a9
Shaun Moss <shaun@astromultimedia.com>
2024-09-26 16:02:07 +1000

    Code style fixes.

--- src/wp-logify/includes/database/models/class-property-array.php
+++ src/wp-logify/includes/database/models/class-property-array.php
@@ -15,7 +15,8 @@ class Property_Array {
 	/**
 	 * See if the array has a property with the given key.
 	 *
-	 * @param string $key The key to search for.
+	 * @param array<string, Property> $props The array of properties to search.
+	 * @param string                  $key   The key to search for.
 	 * @return bool True if the array has a property with the given key, false otherwise.
 	 */
 	public static function has( array $props, string $key ): bool {
@@ -25,7 +26,8 @@ class Property_Array {
 	/**
 	 * Find a property by key.
 	 *
-	 * @param string $key The key to search for.
+	 * @param array<string, Property> $props The array of properties to search.
+	 * @param string                  $key   The key to search for.
 	 * @return ?Property The property object if found, or null if not found.
 	 */
 	public static function get( array $props, string $key ): ?Property {
@@ -36,13 +38,14 @@ class Property_Array {
 	 * Given a property's values, update a property within the array.
 	 * If the property with the given key isn't in the array, create a new one and add it.
 	 *
-	 * @param Property[] $props      The array of properties to update.
-	 * @param string     $key        The property key.
-	 * @param ?string    $table_name The name of the database table the property belongs to.
-	 * @param mixed      $val        The old or current value of the property.
-	 * @param mixed      $new_val    Optional. The new value of the property, if changed.
+	 * @param array<string, Property> $props      The array of properties to update.
+	 * @param string                  $key        The property key.
+	 * @param ?string                 $table_name The name of the database table the property belongs to.
+	 * @param mixed                   $val        The old or current value of the property.
+	 * @param mixed                   $new_val    Optional. The new value of the property, if changed.
+	 * @return void
 	 */
-	public static function set( array &$props, string $key, ?string $table_name, mixed $val, mixed $new_val = null ) {
+	public static function set( array &$props, string $key, ?string $table_name, mixed $val, mixed $new_val = null ): void {
 		if ( key_exists( $key, $props ) ) {
 			// If the array already contains a property with this key, update it with the new values.
 			$props[ $key ]->table_name = $table_name;
@@ -60,10 +63,11 @@ class Property_Array {
 	 * If a property with the given key is already in the array, replace it.
 	 * Otherwise, add the given property to the array.
 	 *
-	 * @param Property[] $props The array of properties to update.
-	 * @param Property   $prop  The Property object to add into the array.
+	 * @param array<string, Property> $props The array of properties to update.
+	 * @param Property                $prop  The Property object to add into the array.
+	 * @return void
 	 */
-	public static function add( array &$props, Property $prop ) {
+	public static function add( array &$props, Property $prop ): void {
 		// Add the new Property object to the array.
 		$props[ $prop->key ] = $prop;
 	}
@@ -72,8 +76,9 @@ class Property_Array {
 	 * Remove a property in an array of properties, searching by key.
 	 *
 	 * @param string $key The key of the property to remove.
+	 * @return void
 	 */
-	public static function remove( array &$props, string $key ) {
+	public static function remove( array &$props, string $key ): void {
 		// Remove the property from the array.
 		unset( $props[ $key ] );
 	}
