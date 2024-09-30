33d51b0274be187d3e012c9c416817f7c8652b1a
Shaun Moss <shaun@astromultimedia.com>
2024-09-30 10:26:18 +1000

    Code improvements to Set class.

--- src/wp-logify/includes/misc/class-set.php
+++ src/wp-logify/includes/misc/class-set.php
@@ -7,10 +7,15 @@
 
 namespace WP_Logify;
 
+use ArrayIterator;
+use Countable;
+use IteratorAggregate;
+use Traversable;
+
 /**
  * Set class. Emulates sets, i.e. unordered collections with no duplicates.
  */
-class Set {
+class Set implements Countable, IteratorAggregate {
 
 	/**
 	 * Items in the set.
@@ -27,19 +32,11 @@ class Set {
 	 *
 	 * Creates a new set containing the values provided.
 	 * Duplicate values will be ignored.
+	 * Arrays will be flattened.
 	 */
 	public function __construct( ...$items ) {
-		$this->items = array_unique( array_values( $items ) );
-	}
-
-	/**
-	 * Create a set from an array.
-	 *
-	 * @param array $arr The array to create the set from.
-	 * @return Set The set created from the array.
-	 */
-	public static function fromArray( array $arr = array() ): Set {
-		return new self( ...$arr );
+		$this->items = array();
+		$this->addItems( $items );
 	}
 
 	// =============================================================================================
@@ -59,7 +56,7 @@ class Set {
 	 *
 	 * @return int The number of items in the set.
 	 */
-	public function count() {
+	public function count(): int {
 		return count( $this->items );
 	}
 
@@ -69,19 +66,38 @@ class Set {
 	 * @param mixed $item The item to check.
 	 * @return bool True if the set contains the item, false otherwise.
 	 */
-	public function contains( $item ) {
-		return in_array( $item, $this->items );
+	public function contains( $item ): bool {
+		return in_array( $item, $this->items, true );
 	}
 
 	/**
 	 * Add an item to the set.
 	 *
 	 * @param mixed $item The item to add.
-	 * @return Set The set with the item added.
+	 * @return self The set with the item added.
 	 */
-	public function add( $item ) {
-		if ( ! in_array( $item, $this->items, true ) ) {
+	public function add( $item ): self {
+		if ( ! $this->contains( $item ) ) {
 			$this->items[] = $item;
+		}
+		return $this;
+	}
+
+	/**
+	 * Add an array of item to the set.
+	 * Note, this method will recursively flatten nested arrays.
+	 * No items added to the set will be arrays.
+	 *
+	 * @param array $items The items to add.
+	 * @return self The set with the items added.
+	 */
+	public function addItems( array $items ): self {
+		foreach ( $items as $item ) {
+			if ( is_array( $item ) ) {
+				$this->addItems( $item );
+			} else {
+				$this->add( $item );
+			}
 		}
 		return $this;
 	}
@@ -90,11 +106,10 @@ class Set {
 	 * Remove an item from the set.
 	 *
 	 * @param mixed $item The item to remove.
-	 * @return Set The set with the item removed.
+	 * @return self The set with the item removed.
 	 */
-	public function remove( $item ) {
-		$this->items = array_values( array_diff( $this->items, array( $item ) ) );
-		return $this;
+	public function remove( $item ): self {
+		return $this->diff( new self( $item ) );
 	}
 
 	/**
@@ -103,7 +118,17 @@ class Set {
 	 * @return bool True if the set is empty, false otherwise.
 	 */
 	public function isEmpty(): bool {
-		return $this->count() === 0;
+		return empty( $this->items );
+	}
+
+	/**
+	 * Clears all items from the set.
+	 *
+	 * @return self The cleared set.
+	 */
+	public function clear(): self {
+		$this->items = array();
+		return $this;
 	}
 
 	// =============================================================================================
@@ -112,31 +137,48 @@ class Set {
 	/**
 	 * Union of two sets.
 	 *
-	 * @param Set $set2 The second set.
-	 * @return Set The union of the two sets.
+	 * @param self $set2 The second set.
+	 * @return self The union of the two sets.
 	 */
-	public function union( self $set2 ): Set {
-		return new self( array_merge( $this->items, $set2->items ) );
+	public function union( self $set2 ): self {
+		// The arrays will be flatted and duplicates will be ignored.
+		return new self( $this->items, $set2->items );
 	}
 
 	/**
 	 * Difference between two sets.
 	 *
-	 * @param Set $set2 The second set.
-	 * @return Set The difference between the two sets.
+	 * @param self $set2 The second set.
+	 * @return self The difference between the two sets.
 	 */
-	public function diff( self $set2 ): Set {
-		return new self( array_diff( $this->items, $set2->items ) );
+	public function diff( self $set2 ): self {
+		// Construct new set containing items from the first set that are not in the second set.
+		// We use this instead of array_diff() because that function does not use strict comparison.
+		$result = new self();
+		foreach ( $this->items as $item ) {
+			if ( ! $set2->contains( $item ) ) {
+				$result->add( $item );
+			}
+		}
+		return $result;
 	}
 
 	/**
 	 * Intersection between two sets.
 	 *
-	 * @param Set $set2 The second set.
-	 * @return Set The intersection between the two sets.
+	 * @param self $set2 The second set.
+	 * @return self The intersection between the two sets.
 	 */
-	public function intersect( self $set2 ): Set {
-		return new self( array_intersect( $this->items, $set2->items ) );
+	public function intersect( self $set2 ): self {
+		// Construct new set containing items from the first set that are also in the second set.
+		// We use this instead of array_intersect() because that function does not use strict comparison.
+		$result = new self();
+		foreach ( $this->items as $item ) {
+			if ( $set2->contains( $item ) ) {
+				$result->add( $item );
+			}
+		}
+		return $result;
 	}
 
 	// =============================================================================================
@@ -145,7 +187,7 @@ class Set {
 	/**
 	 * Checks if two sets are equal.
 	 *
-	 * @param Set $set2 The second set.
+	 * @param self $set2 The second set.
 	 * @return bool True if the sets are equal, false otherwise.
 	 */
 	public function equals( self $set2 ): bool {
@@ -155,7 +197,7 @@ class Set {
 	/**
 	 * Checks if a set is a subset of another set.
 	 *
-	 * @param Set $set2 The second set.
+	 * @param self $set2 The second set.
 	 * @return bool True if the set is a subset of the other set, false otherwise.
 	 */
 	public function isSubset( self $set2 ): bool {
@@ -170,7 +212,7 @@ class Set {
 	/**
 	 * Checks if a set is a proper subset of another set.
 	 *
-	 * @param Set $set2 The second set.
+	 * @param self $set2 The second set.
 	 * @return bool True if the set is a proper subset of the other set, false otherwise.
 	 */
 	public function isProperSubset( self $set2 ): bool {
@@ -180,7 +222,7 @@ class Set {
 	/**
 	 * Checks if a set is a isSuperset of another set.
 	 *
-	 * @param Set $set2 The second set.
+	 * @param self $set2 The second set.
 	 * @return bool True if the set is a superset of the other set, false otherwise.
 	 */
 	public function isSuperset( self $set2 ): bool {
@@ -190,7 +232,7 @@ class Set {
 	/**
 	 * Checks if a set is a proper superset of another set.
 	 *
-	 * @param Set $set2 The second set.
+	 * @param self $set2 The second set.
 	 * @return bool True if the set is a proper superset of the other set, false otherwise.
 	 */
 	public function isProperSuperset( self $set2 ): bool {
@@ -205,12 +247,12 @@ class Set {
 	 *
 	 * @return string The set as a string.
 	 */
-	function toString( $glue = ', ', $left_bracket = '{', $right_bracket = '}' ): string {
+	public function toString( $glue = ', ', $left_bracket = '{', $right_bracket = '}' ): string {
 
 		// Convert the set's item to strings.
 		$items = array_map(
 			function ( $item ) use ( $glue, $left_bracket, $right_bracket ): string {
-				return $item instanceof Set
+				return $item instanceof self
 					? $item->toString( $glue, $left_bracket, $right_bracket )
 					: wp_json_encode( $item );
 			},
@@ -225,7 +267,19 @@ class Set {
 	 *
 	 * @return string The set as a string.
 	 */
-	function __toString(): string {
+	public function __toString(): string {
 		return $this->toString();
+	}
+
+	// =============================================================================================
+	// Iterator methods.
+
+	/**
+	 * Get an iterator for the set.
+	 *
+	 * @return Traversable An iterator for the set.
+	 */
+	public function getIterator(): Traversable {
+		return new ArrayIterator( $this->items );
 	}
 }
